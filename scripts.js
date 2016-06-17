0b87aff63d7df33d3b19ad0579ca1bea4bd797e2
Mohamed YOUNES <mohamed.younes@yandex.com>
2016-06-17 13:06:42 +0100

    added: button hover background-color transition animation

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@ $( document ).ready(function() {
     $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
+    }, 800);
 });
-});
+	$('button').hover(function() {
+        $(this).stop().animate({"color":"#23527c","background-color":"#ddd","border-color": "#ddd"}, 600);
+        }, function () {
+        $(this).stop().animate({"color":"#23527c","background-color":"#aaa","border-color": "#aaa"}, 600);
+    });
+  });
