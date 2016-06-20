41dcfc695279918ee20816967b12a5e97216753e
Mohamed YOUNES <mohamed.younes@yandex.com>
2016-06-20 17:02:55 +0100

    created*

---
+++ scripts.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() { 
+    $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+});
+  $('.content >ul>li:even').css('color','#2B407E');
+  
+	$('button').hover(function() {
+        $(this).stop().animate({"color":"#23527c","background-color":"#ddd","border-color": "#ddd"}, 600);
+        }, function () {
+        $(this).stop().animate({"color":"#23527c","background-color":"#aaa","border-color": "#aaa"}, 600);
+    });
+ $('.round-anchor').hover(function() {
+        $(this).stop().animate({"color":"#eee"}, 300);
+        }, function () {
+        $(this).stop().animate({"color":"#ccc"}, 300);
+ 
+    });
+    });
