8bdd25d42d53e58ec119dec5145c315e0665b64a
Mohamed YOUNES <mohamed.younes@yandex.com>
2016-06-20 16:51:24 +0100

    removed:"img:hover"

--- scripts.js
+++ scripts.js
@@ -1,25 +1,20 @@
-$( document ).ready(function() {
+$( document ).ready(function() { 
     $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 800);
 });
+  $('.content >ul>li:even').css('color','#2B407E');
+  
 	$('button').hover(function() {
         $(this).stop().animate({"color":"#23527c","background-color":"#ddd","border-color": "#ddd"}, 600);
         }, function () {
         $(this).stop().animate({"color":"#23527c","background-color":"#aaa","border-color": "#aaa"}, 600);
     });
-  
-  $('img').hover(function() {
-        $(this).stop().animate({"opacity":1}, 300);
-        }, function () {
-        $(this).stop().animate({"opacity":0.7}, 300);
-    });
-  });
-
  $('.round-anchor').hover(function() {
         $(this).stop().animate({"color":"#eee"}, 300);
         }, function () {
         $(this).stop().animate({"color":"#ccc"}, 300);
-
-  });
+ 
+    });
+    });
